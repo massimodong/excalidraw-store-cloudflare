5d142c9639c21d53e21eed28e3b5c6916f87238b
Massimo Dong <dongmassimo@gmail.com>
2025-04-05 11:38:47 +0800

    check upload file size

--- functions/api/v2/post/index.js
+++ functions/api/v2/post/index.js
@@ -1,12 +1,26 @@
 import { nanoid } from 'nanoid';
 
 export async function onRequest(context) {
+  const SIZE_LIMIT = 26214400; // 25MiB value size upper bound for cloudflare's kv store
   const request = context.request;
   const blob = await request.blob();
   const env = context.env;
 
   const kv_key = nanoid();
   const kv_data = await blob.arrayBuffer();
+
+  if(blob.size > SIZE_LIMIT){
+    const message = JSON.stringify({
+      message: "Could not upload the data.",
+    }, null, 2);
+
+    return new Response(message, {
+      headers: {
+        'content-type': 'application/json; charset=UTF-8',
+      },
+      status: 500,
+    });
+  }
 
   // store the data and return
   await env.kvstore.put(kv_key, kv_data); //TODO: expire time
